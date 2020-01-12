7e0bba49e7441a61e3398ae27c89decf00df1599
Kanjiu Akuma <18683538+KanjiuAkuma@users.noreply.github.com>
2020-01-12 19:31:33 +0100

    Added live time counter

--- lib/live_counter.py
+++ lib/live_counter.py
@@ -4,9 +4,12 @@ Created by Joscha Vack on 1/11/2020.
 
 from time import time
 
+from lib.logger import log_call
 
-class LiveCounter:
+
+class LiveCounter(object):
     def __init__(self, parent):
+        log_call('LiveCounter.__init__')
         self._parent = parent
         self._isLive = False
         self._stream_start = 0
@@ -27,16 +30,20 @@ class LiveCounter:
 
     @property
     def is_live(self):
+        log_call('LiveCounter.is_live.getter')
         return self._isLive
 
     @property
     def seconds_live(self):
+        log_call('LiveCounter.seconds_live.getter')
         return int(self._live_time)
 
     @property
     def minutes_live(self):
+        log_call('LiveCounter.minutes_live.getter')
         return int(self._live_time // 60)
 
     @property
     def hours_live(self):
+        log_call('LiveCounter.hours_live.getter')
         return int(self.minutes_live // 24)
