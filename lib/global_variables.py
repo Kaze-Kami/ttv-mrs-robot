94a6452a26d7b4ab562f1d2f86021178759373fa
Kanjiu Akuma <18683538+KanjiuAkuma@users.noreply.github.com>
2020-01-12 19:30:31 +0100

    Moved extra config from file to code

--- lib/global_variables.py
+++ lib/global_variables.py
@@ -11,7 +11,7 @@ resource_dir = join(dirname(dirname(__file__)), "resources")
 settings_path = join(resource_dir, 'settings.json')
 settings_extra_path = join(resource_dir, 'settings_extra.json')
 whitelist_path = join(resource_dir, 'whitelist.json')
-jackpot_info_path = join(resource_dir, 'jackpot.json')
+jackpot_path = join(resource_dir, 'jackpot.json')
 log_file_path = join(resource_dir, 'log.log')
 
 # ---------------------------
