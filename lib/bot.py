fd2bddb6c1b6af6eaaaa2b0bd11a453a1145bbac
Kanjiu Akuma <18683538+KanjiuAkuma@users.noreply.github.com>
2020-01-14 23:30:51 +0100

    Revert "Merge master into release"

--- lib/bot.py
+++ lib/bot.py
@@ -234,15 +234,15 @@ class Bot(object):
                     log('warn', "Invalid debug command: '%s' (not enough params)" % command)
                     return
                 else:
-                    if 3 < len(command):
+                    if 4 < len(command) or len(command) < 3:
                         log('warn', "Invalid debug command: '%s' (too many params)" % command)
                         return
-                    if 3 == len(command):
-                        user_name = command[1].replace('@', '')
+                    if 4 == len(command):
+                        user_name = command[2].replace('@', '')
                         user_id = self._get_user_id(user_name)
-                        amount = command[2]
+                        amount = command[3]
                     else:
-                        amount = command[1]
+                        amount = command[2]
                     if amount == self._config['core.all_keyword']:
                         amount = self._parent.GetPoints(user_id)
                     else:
