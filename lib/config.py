f9654245b53f5cf50bbc9d3a194e002444c899f9
Kanjiu Akuma <18683538+KanjiuAkuma@users.noreply.github.com>
2020-02-04 17:01:56 +0100

    Fixed issue with disclaimer command

--- lib/config.py
+++ lib/config.py
@@ -53,7 +53,7 @@ def _default_config_data():
         'disclaimer.acknowledge_keyword': 'acknowledge',
         'disclaimer.text.via_whisper': 'You need to send this message via whisper, {user}',
         'disclaimer.text.disable': 'The disclaimer is currently disabled, {user}.',
-        'disclaimer.text.disclaimer': 'Gambling can be addictive. Play responsibly. Underage gambling is an offence. Type {core.acknowledge.command} to acknowledge this disclaimer.',
+        'disclaimer.text.disclaimer': 'Gambling can be addictive. Play responsibly. Underage gambling is an offence. Type {disclaimer.acknowledge_command} to acknowledge this disclaimer.',
         'disclaimer.text.acknowledged': 'You can {gamble.keyword} and {guess.keyword} now {user}.',
         'disclaimer.text.not_acknowledged': 'I\'m sorry {user} but you need to accept the disclaimer first to {gamble.keyword} or {guess.keyword}. Type {disclaimer.command} to display it.',
 
